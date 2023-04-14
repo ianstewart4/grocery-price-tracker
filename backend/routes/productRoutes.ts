11e792a97ed857df6633538a31c354439f15533d
Ian Stewart <ianstewart4@outlook.com>
2023-04-14 15:56:47 -0500

    cleaned up routes

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,7 +1,6 @@
 import express, { Express, Request, Response, Router } from 'express'
 const router = express.Router()
 import { getProducts, setProduct, updateProduct, deleteProduct } from '../controllers/productController'
-// import ProductController from '../controllers/productController'
 
 router.route('/').get(getProducts).post(setProduct)
 router.route('/:id').put(updateProduct).delete(deleteProduct)
--- dist/routes/productRoutes.js
+++ dist/routes/productRoutes.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const productController_1 = require("../controllers/productController");
-// import ProductController from '../controllers/productController'
 router.route('/').get(productController_1.getProducts).post(productController_1.setProduct);
 router.route('/:id').put(productController_1.updateProduct).delete(productController_1.deleteProduct);
 module.exports = router;
