1752ccdcd1f7ecf71619fc76d8c84b1d69270ddb
Ian Stewart <ianstewart4@outlook.com>
2023-04-15 22:50:18 -0500

    started register user

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,24 +1,36 @@
 import { Request, Response } from 'express'
+const jwt = require('jsonwebtoken')
+import jwt from 'jsonwebtoken'
+const bcrypt = require('bcryptjs')
+import bcrypt from 'bcryptjs'
+import asyncHandler from 'express-async-handler'
+import { User } from '../models/userModel'
 
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-export const registerUser = (req: Request, res: Response) => {
+export const registerUser = asyncHandler(async (req: Request, res: Response) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please fill in all fields')
+    }
     res.json({ message: 'Register User' })
-}
+})
 
 // @desc    Authenticate new user
 // @route   POST /api/users/login
 // @access  Public
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
     res.json({ message: 'Login User' })
-}
+})
 
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Public
-export const getMe = (req: Request, res: Response) => {
+export const getMe = asyncHandler(async (req: Request, res: Response) => {
     res.json({ message: 'Get Me' })
-}
+})
 
 
