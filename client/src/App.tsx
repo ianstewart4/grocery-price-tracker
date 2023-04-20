b3d665f07394d5627ace8df1bc8a92c599738a40
Ian Stewart <ianstewart4@outlook.com>
2023-04-20 16:57:05 -0500

    set up front end from scratch with daisyui

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Dashboard() {
-    return (
-        <div>dashboard</div>
-    )
-}
-
-export default Dashboard
