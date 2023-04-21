77370de5340f01d42f76c7200600ae9972a7c68e
Ian Stewart <ianstewart4@outlook.com>
2023-04-21 16:40:59 -0500

    added Provider and store

--- dist/client/src/main.js
+++ dist/client/src/main.js
@@ -6,6 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const client_1 = __importDefault(require("react-dom/client"));
 const App_1 = __importDefault(require("./App"));
+const store_1 = require("./app/store");
+const react_redux_1 = require("react-redux");
+// import * as serviceWorker from './serviceWorker';
 require("./index.css");
-client_1.default.createRoot(document.getElementById('root')).render(react_1.default.createElement(react_1.default.StrictMode, null,
-    react_1.default.createElement(App_1.default, null)));
+client_1.default.createRoot(document.getElementById("root")).render(react_1.default.createElement(react_1.default.StrictMode, null,
+    react_1.default.createElement(react_redux_1.Provider, { store: store_1.store },
+        react_1.default.createElement(App_1.default, null))));
