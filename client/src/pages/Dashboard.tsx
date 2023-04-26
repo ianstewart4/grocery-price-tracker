fba36b6f2c5c662bed705e4e3fdcbe08e6f9c10d
Ian Stewart <ianstewart4@outlook.com>
2023-04-26 16:18:52 -0500

    Removed searchbar

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,13 +1,49 @@
 import * as React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
   return (
     <div>
       <Navbar />
-      <div>
-        <h1>Dashboard</h1>
-      </div>
+      <section className="min-h-screen hero-overlay bg-opacity-60">
+        <h1 className="text-center text-xl pt-10">
+          Welcome {user && user.name}
+        </h1>
+        <p>Trackers Dashboard</p>
+      </section>
+      {/* <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hi there!</h1>
+            <p>Tired of paying too much for your groceries?</p>
+            <p className="mb-5">
+              Well there's nothing I can do about that, but here's a way to keep
+              a little more money in your pocket!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div> */}
+      <Footer />
     </div>
   );
 }
--- dist/client/src/pages/Dashboard.js
+++ dist/client/src/pages/Dashboard.js
@@ -28,10 +28,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = __importStar(require("react"));
 const Navbar_1 = __importDefault(require("../components/Navbar"));
+const Footer_1 = __importDefault(require("../components/Footer"));
+const react_1 = require("react");
+const react_router_dom_1 = require("react-router-dom");
+const react_redux_1 = require("react-redux");
 function Dashboard() {
+    const navigate = (0, react_router_dom_1.useNavigate)();
+    const { user } = (0, react_redux_1.useSelector)((state) => state.auth);
+    (0, react_1.useEffect)(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user, navigate]);
     return (React.createElement("div", null,
         React.createElement(Navbar_1.default, null),
-        React.createElement("div", null,
-            React.createElement("h1", null, "Dashboard"))));
+        React.createElement("section", { className: "min-h-screen hero-overlay bg-opacity-60" },
+            React.createElement("h1", { className: "text-center text-xl pt-10" },
+                "Welcome ",
+                user && user.name),
+            React.createElement("p", null, "Trackers Dashboard")),
+        React.createElement(Footer_1.default, null)));
 }
 exports.default = Dashboard;
