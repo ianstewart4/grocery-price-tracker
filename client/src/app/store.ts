e912e8ad2f893512dd86635eea31915fbcfc73c0
Ian Stewart <ianstewart4@outlook.com>
2023-05-02 11:38:01 -0500

    added products to store

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import trackerReducer from "../features/trackers/trackerSlice";
+import productReducer from "../features/products/productSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     trackers: trackerReducer,
+    products: productReducer,
   },
 });
--- dist/client/src/app/store.js
+++ dist/client/src/app/store.js
@@ -7,9 +7,11 @@ exports.store = void 0;
 const toolkit_1 = require("@reduxjs/toolkit");
 const authSlice_1 = __importDefault(require("../features/auth/authSlice"));
 const trackerSlice_1 = __importDefault(require("../features/trackers/trackerSlice"));
+const productSlice_1 = __importDefault(require("../features/products/productSlice"));
 exports.store = (0, toolkit_1.configureStore)({
     reducer: {
         auth: authSlice_1.default,
         trackers: trackerSlice_1.default,
+        products: productSlice_1.default,
     },
 });
