83facfffae015f797e52cb4b8766c14f59b90792
Ian Stewart <ianstewart4@outlook.com>
2023-05-02 11:39:55 -0500

    brought interfaces back into authService file

--- client/src/features/auth/authService.ts
+++ client/src/features/auth/authService.ts
@@ -1,9 +1,23 @@
 import axios from "axios";
-
 const API_URL = "/api/users/";
 
+interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  token: string;
+}
+
+interface IUserData {
+  user: IUser | null;
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  message: string;
+}
+
 // Register user
-const register = async (userData) => {
+const register = async (userData: IUserData) => {
   const response = await axios.post(API_URL, userData);
 
   if (response.data) {
@@ -14,7 +28,7 @@ const register = async (userData) => {
 };
 
 // Login user
-const login = async (userData) => {
+const login = async (userData: IUserData) => {
   const response = await axios.post(API_URL + "login", userData);
 
   if (response.data) {
