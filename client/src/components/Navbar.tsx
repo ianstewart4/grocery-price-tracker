5a5a58cccc86643f91d68e221d6fce8a3fa2cc03
Ian Stewart <ianstewart4@outlook.com>
2023-07-10 22:42:46 -0500

    removed commented code

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -54,33 +54,6 @@ function Navbar() {
   return (
     <div className="navbar bg-base-100 sticky">
       <div className="navbar-start">
-        {/* <div className="input-group" title="Enter product URL">
-          <input
-            type="text"
-            placeholder="Find Superstore Items"
-            className="input input-bordered w-9/12"
-            // onSubmit={onSubmit}
-            // value={productID}
-            // onChange={(e) => setProduct(e.target.value)}
-          />
-
-          <button className="btn btn-square" type="submit">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div> */}
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           Grocery Price Tracker
         </Link>
